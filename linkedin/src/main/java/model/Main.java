b0676b275122e24b3234342a16eb8bb42686ddf4
Remo Diethelm <remo.diethelm@students.unibe.ch>
2012-03-09 18:12:16 +0100

    first implementation of OAuth service

--- linkedin/src/main/java/model/Main.java
+++ linkedin/src/main/java/model/Main.java
@@ -6,16 +6,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
 		String[] keys = readAPIKeys();
-		System.out.println(keys[0] + " " + keys[1]);
+		OAuthAccess oauth = new OAuthAccess(keys[0], keys[1]);
+		Token accessToken = oauth.getAccessToken();
+		OAuthService service = oauth.getService();
 
+		String url = "http://api.linkedin.com/v1/people/~";
+		OAuthRequest request = new OAuthRequest(Verb.GET, url);
+		service.signRequest(accessToken, request);
+		Response response = request.send();
+		System.out.println(response.getBody());
 	}
 
 	public static String[] readAPIKeys() {
